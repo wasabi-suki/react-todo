db7112363f3e8044342246a0642bb2fd4112282f
wasabi-suki <107193340+wasabi-suki@users.noreply.github.com>
2022-06-15 23:44:31 +0900

    TODOの上限追加

--- src/App.jsx
+++ src/App.jsx
@@ -48,7 +48,12 @@ export const App = () => {
         todoText={todoText}
         onChange={onChangeTodoText}
         onClick={onClickAdd}
+        disabled={incompleteTodos.length >= 5}
       />
+      {incompleteTodos.length >= 5 && (
+        <p style={{ color: "red" }}>登録できるTODOは5個までです。</p>
+      )}
+
       <IncompleteTodos
         todos={incompleteTodos}
         onClickDelete={onClickDelete}
